9d55776787071653c61eaaabf324509f266268e0
Sebastian Mueller <sebm@posteo.de>
2018-11-27 21:49:45 +0000

    first phasing results using PhaseTank for all wt libraries

--- Scripts/run_PhaseTank.r
+++ Scripts/run_PhaseTank.r
@@ -6,19 +6,29 @@ library(stringr)
 baseDir      <- "/projects/nick_matthews"
 segLocation  <- file.path(baseDir, "segmentation_2018")
 gitdir       <- file.path(baseDir, "chlamy_locus_map_github")
+setwd(file.path(gitdir, "PhaseTank_v1.0"))
 
 # selected libraries as discussed with Nick/Seb/Adrian (july2018)
 files <- read_csv(file.path(gitdir, "Summary_of_Data.csv")) %>%
     filter(LociRun2018 == "Yes") %>%
     filter(Controls == "wt")
 
-mylibs <- str_replace(basename(files$File)[1:2], "gff2.gz", "fasta")
-libDir <- file.path(baseDir, "sequencing_data/fasta_all")
-file.path(libDir, mylibs)
-mylibs
-# [1] "SL2108_L1.RUN540.trimmed.filtered_15.non_redundant.v_genome_JGI_assembly5_Chlamydomonas_reinhardtii.patman.fasta"
-# [2] "SL2121_L1.RUNunknown.trimmed.filtered_15.non_redundant.v_genome_JGI_assembly5_Chlamydomonas_reinhardtii.patman.fast
+files$DataCode
+# [1] "SL2108" "SL2121" "SL2122" "SL2123" "SL2124" "SL2125"
+# [7] "SL2181" "SL2182" "SL2183" "SL2184" "SL2185" "SL2186"
+# [13] "SL2187" "SL2188" "SL2189" "SL2301" "SL2302" "SL2303"
+# [19] "SL2310" "SL2311" "SL2312" "SL2313" "SL2314" "SL2315"
+# [25] "SL2322" "SL2323" "SL2324" "SL2325" "SL2326" "SL2327"
+
+libDir <- file.path(baseDir, "sequencing_data/fasta_with_counts")
+mylibs <- file.path(libDir, paste0(files$DataCode,"_assembly5_Chlamydomonas_reinhardtii.patman.aligned_reads.fasta"))
+head(mylibs)
+# [1] "/projects/nick_matthews/sequencing_data/fasta_with_counts/SL2108_assembly5_Chlamydomonas_reinhardtii.patman.aligned_reads.  fasta"
+# [2] "/projects/nick_matthews/sequencing_data/fasta_with_counts/SL2121_assembly5_Chlamydomonas_reinhardtii.patman.aligned_reads.  fasta"
+file.exists(mylibs)
 
 tankcmd <- "perl PhaseTank_v1.0_mod.pl --genome /projects/nick_matthews/resources/Creinhardtii_236.fa --lib"
 
-## needs finishing!!
+system(paste(tankcmd, paste(mylibs, collapse = ",")))
+
+#You can check the output files in directory './OUTPUT_2018.11.27_18.08/'.
